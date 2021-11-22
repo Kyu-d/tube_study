05f53abb77a99aacd7d769d63bd08402b1a9379a
Kyu-d <ldk0421@naver.com>
2021-11-22 23:45:36 +0900

    Add Middleware controller [Add]

--- src/server.js
+++ src/server.js
@@ -5,14 +5,23 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`im in the middle`);
+const logger = (req, res, next) => {
+    // console.log(`im in the middle`);
     // return res.send(`i have the power now`); // end mission
 
-    console.log(`someone is going to: ${req.url}`);
+    console.log(`${req.method} ${req.url}`); // ${req.method} : what method which url > can know
 
     next(); // => req continue.. => middleware next fx
 };
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send(`<h1>Not Allowed</h1>`);
+    } // "/protected" >> cutting processing 
+    console.log(`allowed, you may continue`);
+    next();
+}
 
 const handleHome = (req, res) => {
     return res.send(`<h1>i love you</h1>`); // one of res end method
@@ -23,9 +32,20 @@ const handleLogin = (req, res) => {
     return res.send("you login here");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+    return res.send(`welcome to the private lounge`);
+}
 
-app.get("/login", handleLogin);
+app.use(logger); // important order!! first - use > next - get:url
+// app > req : express => up ~ down loading..... > like a javascript
+// order is very important!
+
+app.use(privateMiddleware);
+
+// app.get("/", gossipMiddleware, handleHome);
+
+app.get("/", handleLogin);
+app.get("/protected", handleProtected);
 
 // request <> respon
 
